a0ea781a62d853ced28be58d14ee1d1d584980ea
abhirampattarkine <abhiram.vjti@gmail.com>
2024-03-21 15:25:34 -0400

    Added struct to store customer information

--- main.go
+++ main.go
@@ -5,58 +5,67 @@ import (
 	"strings"
 )
 
+type customerInfo struct {
+	firstName     string
+	lastName      string
+	email         string
+	ticketsWanted uint
+}
+
 func main() {
 
 	var conferanceName string = "Go Conferance"
 	var remainingTickets uint = 50
-	var firstName string
-	var lastName string
-	var ticketsWanted uint
-	var email string
 
 	var firstNameList = []string{}
+	var customerInfoList = []customerInfo{}
 
 	fmt.Printf("Welcome to our Ticket Selling App for %v !!! \n", conferanceName)
 	fmt.Printf("We have %v remaining tickets \n", remainingTickets)
 
 	for {
 
+		customer := customerInfo{}
+
 		fmt.Println("Please enter your first name")
-		fmt.Scan(&firstName)
+		fmt.Scan(&customer.firstName)
 
 		fmt.Println("Please enter your last name")
-		fmt.Scan(&lastName)
+		fmt.Scan(&customer.lastName)
 
 		fmt.Println("Please enter the number of tickets you want to purchase.")
-		fmt.Scan(&ticketsWanted)
+		fmt.Scan(&customer.ticketsWanted)
 
 		fmt.Println("Please enter your email")
-		fmt.Scan(&email)
+		fmt.Scan(&customer.email)
 
 		// Validate User Inputs
-		isValidName := len(firstName) > 2 && len(lastName) > 2
-		isValidEmail := strings.Contains(email, "@")
-		isValidTicketCount := ticketsWanted > 0 && ticketsWanted <= remainingTickets
+		isValidName := len(customer.firstName) > 2 && len(customer.lastName) > 2
+		isValidEmail := strings.Contains(customer.email, "@")
+		isValidTicketCount := customer.ticketsWanted > 0 && customer.ticketsWanted <= remainingTickets
 
 		if isValidName && isValidEmail && isValidTicketCount {
 
-			firstNameList = append(firstNameList, firstName)
+			firstNameList = append(firstNameList, customer.firstName)
 
-			if ticketsWanted > remainingTickets {
-				fmt.Printf("Only %v tickets are available and you are asking for %v tickets. Please enter correct number \n", remainingTickets, ticketsWanted)
+			if customer.ticketsWanted > remainingTickets {
+				fmt.Printf("Only %v tickets are available and you are asking for %v tickets. Please enter correct number \n", remainingTickets, customer.ticketsWanted)
 				continue
 			} else {
-				remainingTickets = remainingTickets - ticketsWanted
+				remainingTickets = remainingTickets - customer.ticketsWanted
 			}
 
-			fmt.Printf("Your order for %v tickets is now completed %v %v. You will receive confirmation on your email at %v \n", ticketsWanted, firstName, lastName, email)
+			fmt.Printf("Your order for %v tickets is now completed %v %v. You will receive confirmation on your email at %v \n", customer.ticketsWanted, customer.firstName, customer.lastName, customer.email)
 		} else {
 			fmt.Println("You have not enterted correct information. Please enter correct data.")
 			continue
 		}
 
+		customerInfoList = append(customerInfoList, customer)
+
 		fmt.Println("************************")
-		fmt.Printf("List of people who have purchased tickets for conferance are %v\n", firstNameList)
+		fmt.Printf("Name of people who have purchased tickets for conferance are %v\n", firstNameList)
+		fmt.Printf("Complete information of customers who purchased the tickets %v\n", customerInfoList)
 		fmt.Printf("We have %v remaining tickets \n", remainingTickets)
 
 		if remainingTickets == 0 {
